4abe0053a8a6f14e46c9217d7b7bbddf4463a48a
‘Yuan-xl’ <1905434094@qq.com>
2023-05-30 12:34:21 +0800

    edit

--- src/test/java/com/yuakk/ReggieApplicationTests.java
+++ src/test/java/com/yuakk/ReggieApplicationTests.java
@@ -1,13 +1,20 @@
 package com.yuakk;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class ReggieApplicationTests {
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
+    void testRedis() {
+        redisTemplate.opsForValue().set("city", "Beijing");
     }
 
 
